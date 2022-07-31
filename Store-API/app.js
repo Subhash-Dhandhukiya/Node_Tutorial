d6a7f02b998862a92b15a9ddb07fdead9c9a7240
Subhash-Dhandhukiya <subhashdhandhukiya69@gmail.com>
2022-07-31 11:31:38 +0530

    Handle DB Connection

--- Store-API/app.js
+++ Store-API/app.js
@@ -1,1 +1,36 @@
-console.log("Start");
+require("dotenv").config();
+
+const express = require("express");
+const app = express();
+
+const connectDB = require("./db/connect");
+
+const notFoundMiddleware = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
+
+//middleware
+app.use(express.json());
+
+//routes
+app.get("/", (req, res) => {
+  res.send("Hello This is Ready to fire");
+});
+
+//products Routes
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
+const port = process.env.PORT || 3000;
+
+const start = async () => {
+  try {
+    //connect DB
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, console.log(`Server is Listening port ${port}...`));
+  } catch (error) {
+    console.log("Error =>", error);
+  }
+};
+
+start();

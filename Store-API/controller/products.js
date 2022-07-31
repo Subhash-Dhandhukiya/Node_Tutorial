52ca4f741b2eacbbfe90e6c47264449cc7fd7bc2
Subhash-Dhandhukiya <subhashdhandhukiya69@gmail.com>
2022-07-31 17:23:09 +0530

    handle Filter Data from DB

--- Store-API/controller/products.js
+++ Store-API/controller/products.js
@@ -1,10 +1,34 @@
+const Product = require("../models/product");
+
 const getAllProductsStatic = async (req, res) => {
-  throw new Error("Testing aync erros");
-  res.status(200).json({ msg: "Product Testing Route" });
+  const search = "ab";
+
+  const products = await Product.find({
+    name: { $regex: search, $options: "i" },
+  });
+  res.status(200).json({ products, nbHits: products.length });
 };
 
 const getAllProducts = async (req, res) => {
-  res.status(200).json({ msg: "Product Route" });
+  const { featured, company, name } = req.query;
+
+  const queryObject = {};
+
+  if (featured) {
+    queryObject.featured = featured === "true" ? true : false;
+  }
+
+  if (company) {
+    queryObject.company = company;
+  }
+
+  if (name) {
+    queryObject.name = { $regex: name, $options: "i" };
+  }
+
+  console.log("queryObject =>", queryObject);
+  const products = await Product.find(queryObject);
+  res.status(200).json({ products, nbHits: products.length });
 };
 
 module.exports = {

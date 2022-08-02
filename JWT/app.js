66bfc70967805edf564ae54dc6193ac3e197df4d
Subhash-Dhandhukiya <subhashdhandhukiya69@gmail.com>
2022-08-02 22:35:07 +0530

    fix

--- JWT/app.js
+++ JWT/app.js
@@ -3,6 +3,7 @@ require("express-async-errors");
 
 const express = require("express");
 const app = express();
+app.use(express.json());
 
 const mainRouter = require("./routes/main");
 const connectDB = require("./db/connect");
@@ -12,7 +13,6 @@ app.use("/api/v1", mainRouter);
 const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddlewar = require("./middleware/error-handler");
 
-app.use(express.json());
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddlewar);
 
--- JWT/controllers/main.js
+++ JWT/controllers/main.js
@@ -4,7 +4,15 @@
 
 //setup authentication so only the request with JWT can access the dashboard
 
+const CustomAPIError = require("../errors/custom-error");
+
 const login = async (req, res) => {
+  const { username, password } = req.body;
+
+  if (!username || !password) {
+    throw new CustomAPIError("Please provide email and password", 400);
+  }
+
   res.send("Fake Login/Register/SignUp Route");
 };
 

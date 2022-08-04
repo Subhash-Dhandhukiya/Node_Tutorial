ca1334c4b8bd75702078296e208c0d300d5028ff
Subhash-Dhandhukiya <subhashdhandhukiya69@gmail.com>
2022-08-04 22:05:30 +0530

    Final Commit

--- JWT/middleware/error-handler.js
+++ JWT/middleware/error-handler.js
@@ -1,7 +1,10 @@
+const { BadRequestError } = require("../errors");
+const { StatusCodes } = require("http-status-codes");
+
 const errorHandlerMiddleware = async (err, req, res, next) => {
   console.log(err);
   return res
-    .status(500)
+    .status(StatusCodes.INTERNAL_SERVER_ERROR)
     .json({ msg: "Something went wrong, Please Try again" });
 };
 
